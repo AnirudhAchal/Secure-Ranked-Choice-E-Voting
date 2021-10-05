6cf6ecb352edc99b48a8045dfab1c60c8342acaf
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-05 11:13:14 +0530

    Redirect users to login page if not authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import DashboardContainerView from "./components/DashboardContainerView";
 import LoginContainerView from "./components/LoginContainerView";
 import RegisterContainerView from "./components/RegisterContainerView";
 
 function App() {
+  const authenitcated = false;
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={DashboardContainerView} />
+        <Route exact path="/">
+          {authenitcated ? (
+            <DashboardContainerView />
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Route>
         <Route path="/login" component={LoginContainerView} />
         <Route path="/register" component={RegisterContainerView} />
       </Switch>
