107c49d938432c31c920cdae4b97f5669a824fb6
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-11 21:32:17 +0530

    Register urls added to main urls

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/authentication/', include('authentication.urls', namespace='authentication')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
