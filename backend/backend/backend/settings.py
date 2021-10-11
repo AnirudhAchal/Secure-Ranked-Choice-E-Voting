f7eb43213d86320ef25288c413dbe558fcf59c6d
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-11 09:03:33 +0530

    Database settings changed

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
