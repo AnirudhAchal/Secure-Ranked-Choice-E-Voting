4a8d61425b66b04e85cbcbd4ecdf6fcb59331dab
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-12 22:53:02 +0530

    Titles added

--- frontend/src/components/LoginView.jsx
+++ frontend/src/components/LoginView.jsx
@@ -17,36 +17,44 @@ class LoginView extends Component {
     } = this.props;
 
     return (
-      <div className="Login">
-        <Form onSubmit={(e) => onSubmit(e)}>
-          <Form.Group size="lg" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              autoFocus
-              type="email"
-              value={email}
-              onChange={(e) => onEmailChange(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              value={password}
-              onChange={(e) => onPasswordChange(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group size="lg" controlId="login">
-            <Button block size="lg" type="submit" disabled={!validateForm()}>
-              Login
-            </Button>
-          </Form.Group>
-          <Form.Group size="lg" controlId="register">
-            <Form.Label>Don't have an account?&nbsp;</Form.Label>
-            <Link to="/register">Register</Link>
-          </Form.Group>
-        </Form>
-        <NotificationContainer />
+      <div>
+        <h1 className="text-center" style={{ paddingTop: "50px" }}>
+          Welcome to NITK E-Voting!
+        </h1>
+        <h3 className="text-center" style={{ paddingTop: "30px" }}>
+          Please Login to continue.
+        </h3>
+        <div className="Login">
+          <Form onSubmit={(e) => onSubmit(e)}>
+            <Form.Group size="lg" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                autoFocus
+                type="email"
+                value={email}
+                onChange={(e) => onEmailChange(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => onPasswordChange(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group size="lg" controlId="login">
+              <Button block size="lg" type="submit" disabled={!validateForm()}>
+                Login
+              </Button>
+            </Form.Group>
+            <Form.Group size="lg" controlId="register">
+              <Form.Label>Don't have an account?&nbsp;</Form.Label>
+              <Link to="/register">Register</Link>
+            </Form.Group>
+          </Form>
+          <NotificationContainer />
+        </div>
       </div>
     );
   }
