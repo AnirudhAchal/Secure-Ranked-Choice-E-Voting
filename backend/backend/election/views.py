52ce990fa89f556a76424be9ebcfdc94e16c86fb
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-28 23:29:10 +0530

    Election View permissions added

--- backend/backend/election/views.py
+++ backend/backend/election/views.py
@@ -1,23 +1,38 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Election
 from .serializers import ElectionSerializer
 
 
+class ElectionDetailViewPermission(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.voters.all()
+
+
 class CurrentElectionList(generics.ListAPIView):
-    queryset = Election.objects.filter(has_started=True, has_ended=False)
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ElectionSerializer
+
+    def get_queryset(self):
+        return Election.objects.filter(has_started=True, has_ended=False, voters=self.request.user)
 
 
 class UpcomingElectionList(generics.ListAPIView):
-    queryset = Election.objects.filter(has_started=False, has_ended=False)
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ElectionSerializer
+
+    def get_queryset(self):
+        return Election.objects.filter(has_started=False, has_ended=False, voters=self.request.user)
 
 
 class CompletedElectionList(generics.ListAPIView):
-    queryset = Election.objects.filter(has_started=True, has_ended=True)
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ElectionSerializer
 
+    def get_queryset(self):
+        return Election.objects.filter(has_started=True, has_ended=True, voters=self.request.user)
 
-class ElectionDetail(generics.RetrieveAPIView):
+
+class ElectionDetail(generics.RetrieveAPIView, ElectionDetailViewPermission):
+    permission_classes = [permissions.IsAuthenticated, ElectionDetailViewPermission]
     queryset = Election.objects.all()
     serializer_class = ElectionSerializer
