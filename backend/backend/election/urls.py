3522058ac38e8a0a2d600a641dbdf5ff916af6c3
Anirudh Achal <anirudh.achal@gmail.com>
2021-10-28 20:54:16 +0530

    Class views added

--- backend/backend/election/urls.py
+++ backend/backend/election/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-# from .views import ElectionList, ElectionDetail
+from .views import CurrentElectionList, UpcomingElectionList, CompletedElectionList, ElectionDetail
 
 app_name = 'election'
 
 urlpatterns = [
-    # path('<int:pk>/', ElectionDetail.as_view(), name='election_detail'),
-    # path('', ElectionList.as_view(), name='election_list'),
+    path('<int:pk>/', ElectionDetail.as_view(), name='election_detail'),
+    path('current/', CurrentElectionList.as_view(), name='current_election_list'),
+    path('upcoming/', UpcomingElectionList.as_view(), name='upcoming_election_list'),
+    path('completed/', CompletedElectionList.as_view(), name='completed_election_list'),
 ]
--- backend/backend/election/views.py
+++ backend/backend/election/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Election
+# Import serializers
 
-# Create your views here.
+
+class CurrentElectionList(generics.ListAPIView):
+    queryset = Election.objects.filter(has_started=True, has_ended=False)
+    pass
+
+
+class UpcomingElectionList(generics.ListAPIView):
+    queryset = Election.objects.filter(has_started=False, has_ended=False)
+    pass
+
+
+class CompletedElectionList(generics.ListAPIView):
+    queryset = Election.objects.filter(has_started=True, has_ended=True)
+    pass
+
+
+class ElectionDetail(generics.RetrieveAPIView):
+    pass
+
+
+
