5dc486409c202cc7e58c47bfe290f4b469ca3f55
Chaithanya Shyam D <codecraft15@gmail.com>
2021-10-31 11:30:34 +0530

    added graphs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import DashboardContainerView from "./components/DashboardContainerView";
 import LoginContainerView from "./components/LoginContainerView";
 import RegisterContainerView from "./components/RegisterContainerView";
 import ElectionContainerView from "./components/ElectionContainerView";
+import Result from "./components/Result";
 import "react-notifications/lib/notifications.css";
 import AdminDashboardContainerView from "./components/Admin/AdminDashboardContainerView";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/register" component={RegisterContainerView} />
         <Route exact path="/admin" component={AdminDashboardContainerView} />
         <Route path="/election/:id" component={ElectionContainerView} />
+        <Route path="/result" component={Result} />
       </Switch>
     </Router>
   );
