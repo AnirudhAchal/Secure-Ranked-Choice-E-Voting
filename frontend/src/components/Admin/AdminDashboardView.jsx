3a70941686c3949960be2bd1527d97011234786e
muditsinghal <muditsinghal.191cs138@nitk.edu.in>
2021-10-31 11:02:36 +0530

    Newlines added/removed

--- frontend/src/components/Admin/AdminDashboardView.jsx
+++ frontend/src/components/Admin/AdminDashboardView.jsx
@@ -1,14 +1,12 @@
-import React, {useState, useEffect} from 'react'
-import Elections from './Elections';
+import React, { useState, useEffect } from "react";
+import Elections from "./Elections";
 
-function AdminDashboardView(){
-
+function AdminDashboardView() {
   const [upcomingElections, setUpcomingElections] = useState([]);
   const [currentElections, setCurrentElections] = useState([]);
   const [pastElections, setPastElections] = useState([]);
 
   useEffect(() => {
-
     // to axios requests here to get the data about the elections
     setUpcomingElections([
       {
@@ -44,39 +42,47 @@ function AdminDashboardView(){
         votesTotal: 840,
       },
     ]);
-
-
   }, []);
-  
+
   return (
     <div>
-        <div className="container">
-          <h3 className="my-2 d-flex justify-content-center">
-            Ongoing Elections
-          </h3>
-          <hr />
-          <div className="row">{currentElections.map(election => <Elections election={election} />)}</div>
-        </div>
+      <div className="container">
+        <h3 className="my-2 d-flex justify-content-center">
+          Ongoing Elections
+        </h3>
         <hr />
-        <div className="container">
-          <h3 className="my-2 d-flex justify-content-center">
-            Upcoming Elections
-          </h3>
-          <hr />
-          <div className="row">{upcomingElections.map(election => <Elections election={election} />)}</div>
+        <div className="row">
+          {currentElections.map((election) => (
+            <Elections election={election} />
+          ))}
         </div>
+      </div>
+      <hr />
+      <div className="container">
+        <h3 className="my-2 d-flex justify-content-center">
+          Upcoming Elections
+        </h3>
         <hr />
-        <div className="container">
-          <h3 className="my-2 d-flex justify-content-center">
-            Completed Elections
-          </h3>
-          <hr />
-          <div className="row">{pastElections.map(election => <Elections election={election} />)}</div>
+        <div className="row">
+          {upcomingElections.map((election) => (
+            <Elections election={election} />
+          ))}
         </div>
+      </div>
+      <hr />
+      <div className="container">
+        <h3 className="my-2 d-flex justify-content-center">
+          Completed Elections
+        </h3>
+        <hr />
+        <div className="row">
+          {pastElections.map((election) => (
+            <Elections election={election} />
+          ))}
+        </div>
+      </div>
     </div>
   );
-  
 }
 
 export default AdminDashboardView;
-
--- frontend/src/components/Admin/Elections.jsx
+++ frontend/src/components/Admin/Elections.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function Elections({election}) {
-    return (
-        <div className="col-lg-4">
-        <div className="mx-3 card">
-            <div className="card-body">
-            <h5 className="card-title">{election.electionName}</h5>
-            <p className="card-text">{election.electionDescription}</p>
-            <button type="button" className="btn btn-primary">
-                See Election Details
-            </button>
-            </div>
+function Elections({ election }) {
+  return (
+    <div className="col-lg-4">
+      <div className="mx-3 card">
+        <div className="card-body">
+          <h5 className="card-title">{election.electionName}</h5>
+          <p className="card-text">{election.electionDescription}</p>
+          <button type="button" className="btn btn-primary">
+            See Election Details
+          </button>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Elections
+export default Elections;
