fe82122f6440633ce4776de0e41e5ff3a9ad95d7
Anirudh Achal <anirudh.achal@gmail.com>
2021-11-02 00:43:53 +0530

    File restored

--- backend/backend/authentication/views.py
+++ backend/backend/authentication/views.py
@@ -19,24 +19,25 @@ class UserCreate(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
 
-            json = serializer.data
+                # Email verification
+                user = User.objects.get(email=json['email'])
+                token = RefreshToken.for_user(user)
+                current_site = get_current_site(request)
+                relative_link = reverse('authentication:verify_email')
+                absolute_url = f'http://{current_site.domain}{relative_link}?token={str(token.access_token)}'
+                data = {
+                    'domain': absolute_url,
+                    'subject': 'Secure Rank Choice E-Voting - Verify your email',
+                    'body': f'Hi {user.user_name},\nUse link below to verify your email.\n{absolute_url}',
+                    'to_email': f'{user.email}',
+                }
+                Util.send_email(data)
 
-            # Email verification
-            user = User.objects.get(email=json['email'])
-            token = RefreshToken.for_user(user)
-            current_site = get_current_site(request)
-            relative_link = reverse('authentication:verify_email')
-            absolute_url = f'http://{current_site.domain}{relative_link}?token={str(token.access_token)}'
-            data = {
-                'domain': absolute_url,
-                'subject': 'Secure Rank Choice E-Voting - Verify your email',
-                'body': f'Hi {user.user_name},\nUse link below to verify your email.\n{absolute_url}',
-                'to_email': f'{user.email}',
-            }
-            Util.send_email(data)
-
-            return Response(json, status=status.HTTP_201_CREATED)
+                return Response(json, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
