51d171ef2cfb33dc37efd41368b691f03618edeb
muditsinghal <muditsinghal.191cs138@nitk.edu.in>
2021-11-03 17:32:41 +0530

    minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
 import App from "./App";
 
 ReactDOM.render(
