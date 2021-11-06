f8de1ea9e674d6dfcd1c01fc6f52e148ce56e9c7
Anirudh Achal <anirudh.achal@gmail.com>
2021-11-06 19:28:11 +0530

    My profile frontend added

--- frontend/src/components/MyProfileContainerView.jsx
+++ frontend/src/components/MyProfileContainerView.jsx
@@ -11,8 +11,30 @@ class MyProfileContainerView extends Component {
     this.state = {
       user: {},
       userHasLoaded: true,
+      editingFirstName: false,
+      editingLastName: false,
+      editingAbout: false,
+      firstName: "",
+      lastName: "",
+      about: "",
       redirectToLogin: false,
     };
+
+    this.handleSaveFirstName = this.handleSaveFirstName.bind(this);
+    this.handleSaveLastName = this.handleSaveLastName.bind(this);
+    this.handleSaveAbout = this.handleSaveAbout.bind(this);
+
+    this.handleEditFirstName = this.handleEditFirstName.bind(this);
+    this.handleEditLastName = this.handleEditLastName.bind(this);
+    this.handleEditAbout = this.handleEditAbout.bind(this);
+
+    this.handleCancelFirstName = this.handleCancelFirstName.bind(this);
+    this.handleCancelLastName = this.handleCancelLastName.bind(this);
+    this.handleCancelAbout = this.handleCancelAbout.bind(this);
+
+    this.handleChangeFirstName = this.handleChangeFirstName.bind(this);
+    this.handleChangeLastName = this.handleChangeLastName.bind(this);
+    this.handleChangeAbout = this.handleChangeAbout.bind(this);
   }
 
   async componentDidMount() {
@@ -22,14 +44,127 @@ class MyProfileContainerView extends Component {
       });
     }
 
-    const currentUserId = getCurrentUserId();
+    axiosInstance
+      .get(`/authentication/current-user/${getCurrentUserId()}/`)
+      .then((res) => {
+        this.setState({
+          user: res.data,
+          userHasLoaded: true,
+          firstName: res.data.first_name,
+          lastName: res.data.last_name,
+          about: res.data.about,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleEditFirstName() {
+    this.setState({
+      editingFirstName: true,
+    });
+  }
+
+  handleEditLastName() {
+    this.setState({
+      editingLastName: true,
+    });
+  }
+
+  handleEditAbout() {
+    this.setState({
+      editingAbout: true,
+    });
+  }
+
+  handleCancelFirstName() {
+    const { user } = this.state;
+    this.setState({
+      editingFirstName: false,
+      firstName: user.first_name,
+    });
+  }
+
+  handleCancelLastName() {
+    const { user } = this.state;
+    this.setState({
+      editingLastName: false,
+      lastName: user.last_name,
+    });
+  }
+
+  handleCancelAbout() {
+    const { user } = this.state;
+    this.setState({
+      editingAbout: false,
+      about: user.about,
+    });
+  }
+
+  handleChangeFirstName(firstName) {
+    this.setState({
+      firstName: firstName,
+    });
+  }
+
+  handleChangeLastName(lastName) {
+    this.setState({
+      lastName: lastName,
+    });
+  }
+
+  handleChangeAbout(about) {
+    this.setState({
+      about: about,
+    });
+  }
+
+  handleSaveFirstName() {
+    const { user, firstName } = this.state;
+    user.first_name = firstName;
 
     axiosInstance
-      .get(`/authentication/current-user/${currentUserId}/`)
+      .patch(`/authentication/current-user/${getCurrentUserId()}/`, user)
       .then((res) => {
-        //console.log(res.data)
-        this.setState({ user: res.data, userHasLoaded: true });
-        //console.log(this.state.user)
+        console.log(res);
+        this.setState({
+          editingFirstName: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleSaveLastName() {
+    const { user, lastName } = this.state;
+    user.last_name = lastName;
+
+    axiosInstance
+      .patch(`/authentication/current-user/${getCurrentUserId()}/`, user)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          editingLastName: false,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleSaveAbout() {
+    const { user, about } = this.state;
+    user.about = about;
+
+    axiosInstance
+      .patch(`/authentication/current-user/${getCurrentUserId()}/`, user)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          editingAbout: false,
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -37,13 +172,46 @@ class MyProfileContainerView extends Component {
   }
 
   render() {
-    const { redirectToLogin, user, userHasLoaded } = this.state;
+    const {
+      redirectToLogin,
+      user,
+      userHasLoaded,
+      editingFirstName,
+      editingLastName,
+      editingAbout,
+      firstName,
+      lastName,
+      about,
+    } = this.state;
 
     if (redirectToLogin) {
       return <Redirect to="/login" />;
     }
 
-    return <MyProfileView userHasLoaded={userHasLoaded} user={user} />;
+    return (
+      <MyProfileView
+        userHasLoaded={userHasLoaded}
+        user={user}
+        editingFirstName={editingFirstName}
+        editingLastName={editingLastName}
+        editingAbout={editingAbout}
+        firstName={firstName}
+        lastName={lastName}
+        about={about}
+        onChangeFirstName={this.handleChangeFirstName}
+        onChangeLastName={this.handleChangeLastName}
+        onChangeAbout={this.handleChangeAbout}
+        onEditFirstName={this.handleEditFirstName}
+        onEditLastName={this.handleEditLastName}
+        onEditAbout={this.handleEditAbout}
+        onCancelFirstName={this.handleCancelFirstName}
+        onCancelLastName={this.handleCancelLastName}
+        onCancelAbout={this.handleCancelAbout}
+        onSaveFirstName={this.handleSaveFirstName}
+        onSaveLastName={this.handleSaveLastName}
+        onSaveAbout={this.handleSaveAbout}
+      />
+    );
   }
 }
 
--- frontend/src/components/MyProfileView.jsx
+++ frontend/src/components/MyProfileView.jsx
@@ -1,24 +1,130 @@
 import React, { Component } from "react";
 
 class MyProfileView extends Component {
+  renderFirstName() {
+    const {
+      user,
+      firstName,
+      editingFirstName,
+      onChangeFirstName,
+      onSaveFirstName,
+      onCancelFirstName,
+      onEditFirstName,
+    } = this.props;
+
+    if (editingFirstName) {
+      return (
+        <div>
+          <button onClick={onSaveFirstName}>Save</button>
+          <button onClick={onCancelFirstName}>Cancel</button>
+
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={(e) => onChangeFirstName(e.target.value)}
+          />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <button onClick={(e) => onEditFirstName(e)}>Edit</button>
+        <h3>First Name: {user.first_name}</h3>
+      </div>
+    );
+  }
+
+  renderLastName() {
+    const {
+      user,
+      lastName,
+      editingLastName,
+      onChangeLastName,
+      onSaveLastName,
+      onCancelLastName,
+      onEditLastName,
+    } = this.props;
+
+    if (editingLastName) {
+      return (
+        <div>
+          <button onClick={onSaveLastName}>Save</button>
+          <button onClick={onCancelLastName}>Cancel</button>
+
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={(e) => onChangeLastName(e.target.value)}
+          />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <button onClick={(e) => onEditLastName(e)}>Edit</button>
+        <h3>Last Name: {user.last_name}</h3>
+      </div>
+    );
+  }
+
+  renderAbout() {
+    const {
+      user,
+      about,
+      editingAbout,
+      onChangeAbout,
+      onSaveAbout,
+      onCancelAbout,
+      onEditAbout,
+    } = this.props;
+
+    if (editingAbout) {
+      return (
+        <div>
+          <button onClick={onSaveAbout}>Save</button>
+          <button onClick={onCancelAbout}>Cancel</button>
+
+          <input
+            type="text"
+            id="about"
+            name="about"
+            value={about}
+            onChange={(e) => onChangeAbout(e.target.value)}
+          />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <button onClick={(e) => onEditAbout(e)}>Edit</button>
+        <h3>About: {user.about}</h3>
+      </div>
+    );
+  }
+
   render() {
-    const { userHasLoaded } = this.props;
+    const { user, userHasLoaded } = this.props;
 
     if (!userHasLoaded) {
       return null;
     }
 
-    const { user } = this.props;
-    // contians all user details
-    console.log(user);
     return (
       <div>
         <center>
           <h1>Profile Page</h1>
-          <h2>{user.email}</h2>
-          <h2>{user.user_name}</h2>
-          <h2>{user.first_name}</h2>
-          <h2>{user.last_name}</h2>
+          <h3>Email: {user.email}</h3>
+          <h3>Username: {user.user_name}</h3>
+          {this.renderFirstName()}
+          {this.renderLastName()}
+          {this.renderAbout()}
         </center>
       </div>
     );
