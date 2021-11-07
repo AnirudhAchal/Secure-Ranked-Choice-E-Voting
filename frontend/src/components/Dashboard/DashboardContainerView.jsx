6c5f2877218739ff6ff81e9b651f701f4cf6bd95
Anirudh Achal <anirudh.achal@gmail.com>
2021-11-07 19:09:14 +0530

    Constructor deleted

--- frontend/src/components/Dashboard/DashboardContainerView.jsx
+++ frontend/src/components/Dashboard/DashboardContainerView.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import DashboardView from "./DashboardView";
 
 class DashboardContainerView extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return <DashboardView />;
   }
