79b9d5a78971e9613e62da832aa42c52b3455885
Anirudh Achal <anirudh.achal@gmail.com>
2021-11-08 23:40:37 +0530

    Bug fixed

--- backend/backend/election/views.py
+++ backend/backend/election/views.py
@@ -82,30 +82,32 @@ class ElectionDetail(generics.RetrieveAPIView, ElectionDetailViewPermission):
 
     def get_queryset(self):
         election_id = self.kwargs['pk']
-        election = Election.objects.get(pk=election_id)
 
-        # If election is completed we need to check if the results have been tabulated
-        # and tally up the vote if they haven't already been counted
-        if timezone.now() > election.end_date:
+        if Election.objects.filter(pk=election_id).exists():
+            election = Election.objects.get(pk=election_id)
 
-            # If election details are NULL then initialize object
-            if not election.election_details:
-                election.election_details = {}
+            # If election is completed we need to check if the results have been tabulated
+            # and tally up the vote if they haven't already been counted
+            if timezone.now() > election.end_date:
 
-            # If results not in election_details then calculate result using rank choice
-            if 'results' not in election.election_details:
-                ballot_objects = Ballot.objects.filter(election=election)
-                ballots = []
-                for ballot_object in ballot_objects:
-                    ballots.append(ballot_object.vote_details['preferences'])
+                # If election details are NULL then initialize object
+                if not election.election_details:
+                    election.election_details = {}
 
-                results = Util.get_rank_choice_results(ballots=ballots)
-                election.election_details['results'] = results
+                # If results not in election_details then calculate result using rank choice
+                if 'results' not in election.election_details:
+                    ballot_objects = Ballot.objects.filter(election=election)
+                    ballots = []
+                    for ballot_object in ballot_objects:
+                        ballots.append(ballot_object.vote_details['preferences'])
 
-                if results['winner']:
-                    election.winner = get_user_model().objects.get(pk=results['winner'])
+                    results = Util.get_rank_choice_results(ballots=ballots)
+                    election.election_details['results'] = results
 
-                election.save()
+                    if results['winner']:
+                        election.winner = get_user_model().objects.get(pk=results['winner'])
+
+                    election.save()
 
         return Election.objects.all()
 
